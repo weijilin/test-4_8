703ce71845250344bf9aa3a6dd688348ed9bdbef
weijilin <472442433@qq.com>
2019-08-02 15:34:24 +0800

    提交代码

---
+++ test-4_8/源.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int i = 1, b = 1, a = 7;
+//	for (i = 1; i <= a; i ++){
+//		for (b= 1; b<= 2*i-1; b++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = a-1; i >=1; i--){
+//		for (b = 1; b <= 2 * i - 1; b++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	
+//	system("pause");
+//	return 0;
+//}
+#include<math.h>
+//int main()
+//{
+//	int i = 100, w = 999;
+//	int a, b, c, s = 0;
+//	for (i = 100; i <= w; i++){
+//		a = i / 100;
+//		b = (i - a * 100) / 10;
+//		c = i % 10;
+//		s = a*a*a + b*b*b + c*c*c;
+//		if (i == s){
+//			printf("%d ", s);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+int main(){
+	int a;
+	scanf("%d", &a); 
+	printf("%d", a);
+	system("pause");
+	return 0;
+}
